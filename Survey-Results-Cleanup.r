62fc7f5d01943aa415c97cd7039934e68a2a4ce4
DanielBrett <44175011+DanielBrett@users.noreply.github.com>
2025-01-24 09:34:25 -0500

    Updated R script with better axis labels

--- Survey-Results-Cleanup.r
+++ Survey-Results-Cleanup.r
@@ -6,7 +6,7 @@ sdata <- read.csv("Coffee-Survey-Results.csv")
 ggplot(data = sdata)+geom_bar(mapping = aes(x=coffee.daily))
 ggsave("daily-coffee-drinkers.jpg", width = 8, height = 6, dpi = 300, device = "jpeg")
 
-ggplot(data = sdata)+geom_bar(mapping = aes(x=num.cups))+labs(x="Number of Cups Per Day",y="Count",title="How Many Coffee's Drunk Per Day")+scale_y_continuous(breaks =c(1,2,3,4,5,6,7,8,9))
+ggplot(data = sdata)+geom_bar(mapping = aes(x=num.cups))+labs(x="Number of Cups Per Day",y="Number of People",title="How Many Coffee's Drunk Per Day")+scale_y_continuous(breaks =c(1,2,3,4,5,6,7,8,9))
 ggsave("coffees-per-day.jpg", width = 8, height = 6, dpi = 300, device = "jpeg")
 
 ggplot(data = sdata)+geom_bar(mapping = aes(x=morning))
